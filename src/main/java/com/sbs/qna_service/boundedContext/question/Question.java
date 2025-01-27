d2ffdb474e5983a798f71047a94380ca8af0777f
ohjungminbdkk <bdkk0209@hotmail.com>
2025-01-27 20:17:36 +0900

    Question @OneToMany setting

--- src/main/java/com/sbs/qna_service/boundedContext/question/Question.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/Question.java
@@ -1,13 +1,17 @@
 package com.sbs.qna_service.boundedContext.question;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import com.sbs.qna_service.boundedContext.answer.Answer;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +30,9 @@ public class Question {
     private String content; //内容
 
     private LocalDateTime createDate;
+    
+    //CascadeType.REMOVE 質問が削除されたら、コメントも削除される。
+    //これは、DBテーブルにはカラムが生成されない。（DBはリストや配列ができない。）
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
