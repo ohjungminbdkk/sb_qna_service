0ac3aae6177a2aa26c57b8c290b12caa6aea27e6
ohjungminbdkk <bdkk0209@hotmail.com>
2025-01-30 17:07:09 +0900

    답변등록

---
+++ src/main/java/com/sbs/qna_service/boundedContext/answer/AnswerController.java
@@ -0,0 +1,31 @@
+package com.sbs.qna_service.boundedContext.answer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sbs.qna_service.boundedContext.question.Question;
+import com.sbs.qna_service.boundedContext.question.QuestionService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+	private final QuestionService questionService;
+	
+	private final AnswerService answerService;
+
+	@PostMapping("/create/{id}")
+	public String createAnswer(Model model, @PathVariable("id") Integer id,
+			@RequestParam(value = "content") String content) {
+		Question question = this.questionService.getQuestion(id);
+		// TODO: 답변을 저장한다.
+		this.answerService.create(question, content);
+		return String.format("redirect:/question/detail/%s", id);
+	}
+}
