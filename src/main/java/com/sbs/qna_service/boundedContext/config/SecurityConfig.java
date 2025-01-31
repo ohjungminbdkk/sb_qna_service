017d86d3f95ad5ff7eeaa35b0b39c7b959b149e3
ohjungminbdkk <bdkk0209@hotmail.com>
2025-01-31 19:31:52 +0900

    중복된 가입 금지

--- src/main/java/com/sbs/qna_service/boundedContext/config/SecurityConfig.java
+++ src/main/java/com/sbs/qna_service/boundedContext/config/SecurityConfig.java
@@ -6,16 +6,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-        ;
-        return http.build();
-    }
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+				.requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+		return http.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/sbs/qna_service/boundedContext/user/UserService.java
+++ src/main/java/com/sbs/qna_service/boundedContext/user/UserService.java
@@ -1,6 +1,6 @@
 package com.sbs.qna_service.boundedContext.user;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -11,12 +11,14 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+    
     public SiteUser create(String username, String email, String password) {
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // Bcrypt 암호화 알고리즘
-        user.setPassword(passwordEncoder.encode(password));
+        //BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // Bcrypt 암호화 알고리즘
+        user.setPassword(passwordEncoder.encode(password));//BcryptPasswordEncoder 객체를 직접 생성하여 사용하지 않고 빈으로 등록한 Password Encoder 객체를 주입받아 사용할 수 있도록 수정
         this.userRepository.save(user);
         return user;
     }
