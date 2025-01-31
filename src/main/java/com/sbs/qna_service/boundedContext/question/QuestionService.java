ae779cdb49763c98026c257a31171af12ce756c2
ohjungminbdkk <bdkk0209@hotmail.com>
2025-01-31 15:21:29 +0900

    작성일시 역순으로 조회

--- src/main/java/com/sbs/qna_service/boundedContext/question/QuestionService.java
+++ src/main/java/com/sbs/qna_service/boundedContext/question/QuestionService.java
@@ -1,12 +1,14 @@
 package com.sbs.qna_service.boundedContext.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.sbs.qna_service.boundedContext.DataNotFoundException;
@@ -42,7 +44,12 @@ public class QuestionService {
 	}
 
 	public Page<Question> getList(int page) {
-		Pageable pageable = PageRequest.of(page, 10);
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate"));// 작성일자 순으로 정렬
+		// page : 요청된 페이지 번호(0부터 시작)
+		// 10 : 한 페이지에 표시할 데이터 갯수
+		// Sort.by(sorts) : 전렬 기준, 'createDate'를 기준을 적용
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 한페이지에 10개씩
 		return questionRepository.findAll(pageable);
 	}
 }
